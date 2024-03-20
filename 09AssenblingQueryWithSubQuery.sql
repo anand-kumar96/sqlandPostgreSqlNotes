702a4e9e8727e43943de8e39c6bfea3066cd509e
Anand kumar <106487247+anand-kumar96@users.noreply.github.com>
2024-03-20 12:39:58 +0530

    Update 09AssenblingQueryWithSubQuery.sql

--- 09AssenblingQueryWithSubQuery.sql
+++ 09AssenblingQueryWithSubQuery.sql
@@ -1,4 +1,5 @@
---> 1 : What's a Subquery? 
+--> 1 : What's a Subquery? : A subquery in SQL is a nested query enclosed within parentheses, used to retrieve data necessary for the main query to execute.
+                         --> It can appear in various parts of a SQL statement and is commonly used for filtering, joining, or performing calculations.
   --> Question: List the name and price of all products that are more expensive than all products in the 'Toys' department.
   --> Solution: https://github.com/anand-kumar96/sqlandPostgreSqlNotes/assets/106487247/581c0d50-acb1-4fb9-8ffe-501fdaaf60c8
   --> First find most expensive product of toys then find all products which is more expensive than this.
@@ -24,9 +25,37 @@
       from products
       where department = 'Toys'
       );
+
 --> 2 : Thinking about structure of data ? --> Very very Important
+    --> 1 : 1=> SUBQUERY in SELECT 
     --> https://github.com/anand-kumar96/sqlandPostgreSqlNotes/assets/106487247/ec8fa3e9-8344-408d-b8c9-bce0ad62eab9
     --> https://github.com/anand-kumar96/sqlandPostgreSqlNotes/assets/106487247/a34bd8a7-0b54-41fa-8504-12df1d1764e6
     --> https://github.com/anand-kumar96/sqlandPostgreSqlNotes/assets/106487247/75586833-6d63-4991-ab74-6e5f488537b3
+    --> Example:
+        SELECT price, name, (
+          SELECT price 
+          FROM products
+          WHERE id = 3
+       ) As id_3_price
+       FROM products
+       WHERE price > 867;
 
-
+--> 3 : Exercise 
+    --> https://github.com/anand-kumar96/sqlandPostgreSqlNotes/assets/106487247/841f604c-055b-4745-8c3b-b8c92cbdf118
+--> 4 : Solution
+        SELECT name, price, 
+         price/(
+         Select Max(price)
+         FROM phones) AS price_ratio
+        FROM phones;
+   
+--> 5 : 2=> SUBQUERY in FROM 
+        https://github.com/anand-kumar96/sqlandPostgreSqlNotes/assets/106487247/ebd14b5b-0408-4422-9192-6a1c84e1d625
+        https://github.com/anand-kumar96/sqlandPostgreSqlNotes/assets/106487247/2432de42-f707-4190-b89c-de69cb2097dc
+    --> Query
+        SELECT name, price_weight_ratio
+        FROM (
+          SELECT name, price / weight AS price_weight_ratio
+          FROM products
+          ) As p 
+        WHERE price_weight_ratio > 5;
