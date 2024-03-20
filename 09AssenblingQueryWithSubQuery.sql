025660db26ca8bcfa7557343b04c1d27d1902f04
Anand Kumar <hyeiamaka06@gmail.com>
2024-03-20 12:44:24 +0530

    updated

--- 09AssenblingQueryWithSubQuery.sql
+++ 09AssenblingQueryWithSubQuery.sql
@@ -1,30 +1,31 @@
---> 1 : What's a Subquery? : A subquery in SQL is a nested query enclosed within parentheses, used to retrieve data necessary for the main query to execute.
-                         --> It can appear in various parts of a SQL statement and is commonly used for filtering, joining, or performing calculations.
-  --> Question: List the name and price of all products that are more expensive than all products in the 'Toys' department.
-  --> Solution: https://github.com/anand-kumar96/sqlandPostgreSqlNotes/assets/106487247/581c0d50-acb1-4fb9-8ffe-501fdaaf60c8
-  --> First find most expensive product of toys then find all products which is more expensive than this.
-      Select name, price
-      From products 
-      Where price > (most Expensive Product);
-  --> Final Solution : https://github.com/anand-kumar96/sqlandPostgreSqlNotes/assets/106487247/a64dac34-d5e0-442b-bfc2-ff56519bc342
-      Select Max(price)
-      from products
-      where department = 'Toys';
+--> 1 : What's a Subquery?
+    --> A subquery in SQL is a nested query enclosed within parentheses, used to retrieve data necessary for the main query to execute.
+    --> It can appear in various parts of a SQL statement and is commonly used for filtering, joining, or performing calculations.
+    --> Question: List the name and price of all products that are more expensive than all products in the 'Toys' department.
+    --> Solution: https://github.com/anand-kumar96/sqlandPostgreSqlNotes/assets/106487247/581c0d50-acb1-4fb9-8ffe-501fdaaf60c8
+    --> First find most expensive product of toys then find all products which is more expensive than this.
+        Select name, price
+        From products 
+        Where price > (most Expensive Product);
+    --> Final Solution : https://github.com/anand-kumar96/sqlandPostgreSqlNotes/assets/106487247/a64dac34-d5e0-442b-bfc2-ff56519bc342
+        Select Max(price)
+        from products
+        where department = 'Toys';
 
-      Select name, price 
-      from products 
-      where price > --- above query put
-  --> So whenever we have to write two seperate query and sending them to server or database --> instead we combine this two query in single query using ==> SUBQUERY
-  --> SUBQUERY: Subquery can be simply defined as a query within another query. In other words Subquery is a query that is embedded in WHERE clause of another SQL query. 
-  --> Important rules for Subqueries: You can place the Subquery in a number of SQL clauses: WHERE clause, HAVING clause, FROM clause.
-  --> how to write ?
-      Select name, price 
-      from products 
-      where price > (
-      Select Max(price)
-      from products
-      where department = 'Toys'
-      );
+        Select name, price 
+        from products 
+        where price > --- above query put
+    --> So whenever we have to write two seperate query and sending them to server or database --> instead we combine this two query in single query using ==> SUBQUERY
+    --> SUBQUERY: Subquery can be simply defined as a query within another query. In other words Subquery is a query that is embedded in WHERE clause of another SQL query. 
+    --> Important rules for Subqueries: You can place the Subquery in a number of SQL clauses: WHERE clause, HAVING clause, FROM clause.
+    --> how to write ?
+        Select name, price 
+        from products 
+        where price > (
+        Select Max(price)
+        from products
+        where department = 'Toys'
+        );
 
 --> 2 : Thinking about structure of data ? --> Very very Important
     --> 1 : 1=> SUBQUERY in SELECT 
@@ -36,9 +37,9 @@
           SELECT price 
           FROM products
           WHERE id = 3
-       ) As id_3_price
-       FROM products
-       WHERE price > 867;
+        ) As id_3_price
+        FROM products
+        WHERE price > 867;
 
 --> 3 : Exercise 
     --> https://github.com/anand-kumar96/sqlandPostgreSqlNotes/assets/106487247/841f604c-055b-4745-8c3b-b8c92cbdf118
