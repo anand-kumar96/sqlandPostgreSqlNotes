27c1874de9e6b795ad4720c7734252e3800ce966
Anand Kumar <hyeiamaka06@gmail.com>
2024-03-22 17:52:35 +0530

    updated

--- 07SortingRecords.sql
+++ 07SortingRecords.sql
@@ -1,30 +1,30 @@
---> 1 : Sorting by price
+--> 01: Sorting by price
         SELECT * 
         FROM products
         ORDER BY price;
-  --> : Decreasing order
+    --> Decreasing order
         SELECT * 
         FROM products
         ORDER BY price DESC;
 
---> 2 : Sorting based on string
+--> 02: Sorting based on string
         SELECT * 
         FROM products
         ORDER BY name;
-  --> : Decreasing order
+    --> Decreasing order
         SELECT * 
         FROM products
         ORDER BY name DESC;
--->  :  Sorting when price is same then sort by weight
+    --> Sorting when price is same then sort by weight
         SELECT *
         FROM products
         ORDER BY price ASC, weight ASC;
-  --> : Decreasing order
+    --> Decreasing order
         SELECT *
         FROM products
         ORDER BY price ASC, weight DESC;
 
---> 3 : The 'OFFSET' syntax in SQL is a clause used to skip a certain number of rows when returning the result set of a query. 
+--> 03: The 'OFFSET' syntax in SQL is a clause used to skip a certain number of rows when returning the result set of a query. 
     --> It's typically used with the 'LIMIT' clause to handle data pagination in SQL queries efficiently.
     --> The 'LIMIT' option allows you to limit the number of rows returned from a query.
     --> Execlude first 5 products
@@ -48,18 +48,20 @@
         ORDER BY price 
         LIMIT 20
         OFFSET x; --> where x--> 0--20--30--40--60--80--100----
---> 4 : Sorting, Offsetting and Limiting Question
-  --> : Question: Write a query that shows the names of only second and third most expensive phones.
---> 5 : Solutions
-  --> : Creating phones table and inserting record
+
+--> 04: Sorting, Offsetting and Limiting Question
+    --> Question: Write a query that shows the names of only second and third most expensive phones.
+
+--> 05: Solutions
+    --> Creating phones table and inserting record
         CREATE TABLE phones(
           id serial PRIMARY KEY,
           name VARCHAR(20),
           manufacturer VARCHAR(200),
           price INTEGER,
           units_sold INTEGER
-          );
-
+        );
+    --> Inserting data
         INSERT INTO phones (name, manufacturer, price,units_sold)
         VALUES
            ('N1280','Nokia',199,1925),
@@ -68,7 +70,7 @@
            ('S5620 Monte','Samsung',250,2385),
            ('N8','Nokia',150,7543);
 
---> 6 : Query:-> Solution
+--> 06: Query:-> Solution
         SELECT name
         FROM phones
         ORDER BY price DESC
