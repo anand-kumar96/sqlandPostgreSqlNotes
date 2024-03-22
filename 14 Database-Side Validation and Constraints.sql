3f1226e68fd7a7166ec5f79a3f85f19b35f25c81
Anand Kumar <hyeiamaka06@gmail.com>
2024-03-22 12:55:41 +0530

    updated

--- 14 Database-Side Validation and Constraints.sql
+++ 14 Database-Side Validation and Constraints.sql
@@ -12,7 +12,7 @@
     --> to see table--> click on validation database --> expand it click on Schemas--> public -->Tables --> products(table name) --> right click => view/edit data
         INSERT INTO products(name,department,price,weight)
         VALUES ('Shirt','Clothes',200,1);
-        
+
 --> 03: Applying null constraints 
         INSERT INTO products(name,department,weight)
         VALUES ('Pants','Clothes',3);
@@ -33,4 +33,37 @@
     --> ERROR:  column "price" of relation "products" contains null values 
     --> so we can either select all rows which contains price null and delete them or update them
 
---> 04: 
+--> 04: so we can solve it two way : before creating table or after creating table => setting default value
+        CREATE TABLE (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(50) NOT NULL,
+        department VARCHAR(50) NOT NULL,
+        price INTEGER DEFAULT 9999,
+        weight INTEGER 
+        );
+    --> or After Creating Table
+        ALTER TABLE products
+        ALTER COLUMN price
+        SET DEFAULT 9999;
+    --> Now we can add new value
+        INSERT INTO products(name,department,weight)
+        VALUES ('Gloves','Tools',1);
+
+--> 05: Applying a unique constrains to a column
+        INSERT INTO products(name,department,price,weight)
+        VALUES ('Shirt','Clothes',200,1);
+    --> we have two products with same name --> but we want name should be unique how to do that?
+    --> so we use UNIQUE constraints
+    --> When creating table
+        CREATE TABLE (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(50) UNIQUE,
+        department VARCHAR(50) NOT NULL,
+        price INTEGER DEFAULT 9999,
+        weight INTEGER 
+        );
+    --> After Creating Table
+        ALTER TABLE products
+        ADD UNIQUE(name);
+    --> we can't execute this command unless all the value in name column is unique.
+    --> ERROR: could not create unique index "products_name_key" DETAIL: Key (name)=(Shirt) is duplicated.
