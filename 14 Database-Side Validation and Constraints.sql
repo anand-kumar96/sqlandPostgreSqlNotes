072972c5f5497ad70fa79bc0eec0184bac1bf853
Anand Kumar <hyeiamaka06@gmail.com>
2024-03-22 12:33:29 +0530

    updated

---
+++ 14 Database-Side Validation and Constraints.sql
@@ -0,0 +1,36 @@
+--> 1&2:https://github.com/anand-kumar96/sqlandPostgreSqlNotes/assets/106487247/bdc59d78-06f4-4259-a847-e7cc3a2d33bc
+    --> Create product table and insertdata in database  
+    --> Create validation database and open query tols when we open query tools then we are using a specific databse
+    --> https://github.com/anand-kumar96/sqlandPostgreSqlNotes/assets/106487247/ff797693-3ee4-4752-9f39-1f8409825828
+        CREATE TABLE (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(50),
+            department VARCHAR(50),
+            price INTEGER,
+            weight INTEGER
+        );
+    --> to see table--> click on validation database --> expand it click on Schemas--> public -->Tables --> products(table name) --> right click => view/edit data
+        INSERT INTO products(name,department,price,weight)
+        VALUES ('Shirt','Clothes',200,1);
+        
+--> 03: Applying null constraints 
+        INSERT INTO products(name,department,weight)
+        VALUES ('Pants','Clothes',3);
+    --> it will add price as null : that is big issue so we not some type of validation 
+    --> so we can solve it two way : before creating table or after creating table
+    --> https://github.com/anand-kumar96/sqlandPostgreSqlNotes/assets/106487247/88bd33e8-8ae5-461e-8465-4e6db2b30616
+            CREATE TABLE (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(50),
+            department VARCHAR(50),
+            price INTEGER NOT NULL,
+            weight INTEGER
+        );
+    --> or
+        ALTER TABLE products
+        ALTER COLUMN price
+        SET NOT NULL;
+    --> ERROR:  column "price" of relation "products" contains null values 
+    --> so we can either select all rows which contains price null and delete them or update them
+
+--> 04: 
