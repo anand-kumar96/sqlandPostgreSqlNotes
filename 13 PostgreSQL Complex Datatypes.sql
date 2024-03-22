c257767ff2adf4d2ed7e7ca1819b84947f5e1aee
Anand Kumar <hyeiamaka06@gmail.com>
2024-03-22 11:23:01 +0530

    updated

--- 13 PostgreSQL Complex Datatypes.sql
+++ 13 PostgreSQL Complex Datatypes.sql
@@ -36,4 +36,31 @@
         SELECT(null:: Boolean) --> NULL
 
 --> 07: Times, Date and TimesStamp : https://github.com/anand-kumar96/sqlandPostgreSqlNotes/assets/106487247/ee36e3c8-2c76-49a7-ac46-8933f208358c
+    --> DATE    
+        SELECT('1980-11-20'::DATE) --> 1980-11-20
+        SELECT('Nov-20-1980'::DATE) --> 1980-11-20
+        SELECT('20-Nov-1980'::DATE) --> 1980-11-20
+        SELECT('1980-November-20'::DATE) --> 1980-11-20
+        SELECT('November 20,1980'::DATE) --> 1980-11-20
         
+    --> TIME: TIME WITHOUT TIME ZONE --> 24 hour format  
+        SELECT('01:23 AM'::TIME) --> 01:23:00
+        SELECT('05:23 PM'::TIME) --> 17:23:00
+        SELECT('20:34'::TIME) --> 20:34:00
+
+    --> TIME: TIME WITH TIME ZONE --> 24 hour format : https://github.com/anand-kumar96/sqlandPostgreSqlNotes/assets/106487247/f6738aee-282a-4792-b0e3-99d3ef5fdeb8
+        SELECT('01:23 AM EST'::TIME WITH TIME ZONE) --> 01:23:00-05:00 -> Mwans 5 hours behind UTC time zone
+        SELECT('05:23 PM PST'::TIME WITH TIME ZONE) --> 17:23:00-08:00
+        SELECT('05:23 PM UTC'::TIME WITH TIME ZONE) --> 17:23:00+00:00
+    
+    --> TIMESTAMP WITH TIME ZONE
+        SELECT('Nov-20-1980 05:23 PM PST':: TIMESTAMP WITH TIME ZONE) --> 1980-11-21 06:53:00+05:30
+        
+--> 08: INTERVAL
+    --> 1 day--> 1 day
+    --> 1 D --> 1 day
+    --> 1 D 1 M 1 S --> 1 day 1 min 1 second
+        SELECT('1 day':: INTERVAL) --> 1 day
+        SELECT('1 D':: INTERVAL) --> 1 day
+        SELECT('1 D 1 M 1 S':: INTERVAL) --> 1 day 00:01:01
+        SELECT('NOV-20-1980 1:23 AM IST':: TIMESTAMP WITH TIME ZONE) - ('1 D'::INTERVAL); --> 1980-11-19 04:53:00+05:30
