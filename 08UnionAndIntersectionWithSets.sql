23bac4fda4f423b532153f59222d97e6d06f6c52
Anand Kumar <hyeiamaka06@gmail.com>
2024-03-22 17:43:52 +0530

    updated

--- 08UnionAndIntersectionWithSets.sql
+++ 08UnionAndIntersectionWithSets.sql
@@ -30,11 +30,11 @@
     --> id	        name           	        department         price  weight
         38    	   Awesome Fresh Keyboard	     Home	        982	   30
         86         Refined Concrete Pants	     Sports         724	   2
-        46 	    Incredible Granite Bacon      Music	        982    9
+        46 	   Incredible Granite Bacon         Music	        982    9
         80         Small Fresh Gloves	         Garden         991    8
-        24	    Small Plastic Soap	         Beauty         345    1
-        7	    Incredible Granite Mouse      Home           989	   2
-        1	    Practical Fresh Shirt	     Toys           876	   3
+        24	   Small Plastic Soap	            Beauty         345    1
+        7	   Incredible Granite Mouse         Home           989	   2
+        1	   Practical Fresh Shirt	       Toys           876	   3
 
     --> But there is only 7 products becoz 1 product is common in both list.
     --> But if donot want to remove duplicate then use : UnionAll : UNION ALL combines the results of two or more SELECT statements, showing all values, including duplicates if they exist.
