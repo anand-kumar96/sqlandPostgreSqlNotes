b70a704a97b9fa684694d72bfb9fb78a471ed94f
Anand Kumar <hyeiamaka06@gmail.com>
2024-03-22 10:32:05 +0530

    updated

--- 13 PostgreSQL Complex Datatypes.sql
+++ 13 PostgreSQL Complex Datatypes.sql
@@ -3,6 +3,7 @@
     --> Can connect to local or remote database.
     --> Can view/change just about anything in PG.
     --> https://github.com/anand-kumar96/sqlandPostgreSqlNotes/assets/106487247/bc1b0c4e-6e92-45bd-9285-a62aff667125
+
 --> 02: Data Types
     --> https://github.com/anand-kumar96/sqlandPostgreSqlNotes/assets/106487247/c5906513-addc-456d-8683-6aef2909838b
     --> https://github.com/anand-kumar96/sqlandPostgreSqlNotes/assets/106487247/2b802199-cd44-43f8-a53c-6f2e6338cc32
@@ -13,8 +14,26 @@
 
 --> 03: Fast Rule on Storing Numbers
     --> https://github.com/anand-kumar96/sqlandPostgreSqlNotes/assets/106487247/dfe12501-d4f7-4214-accb-32677e8153fa
+
 --> 04: Play with Query Editor
         Select (2.0); --> ?column? numeric 2.o
         SELECT (2.0 :: INTEGER); --> int4 Integer 2
         SELECT (2.0 :: SMALLINT);
         SELECT (1.9999 :: DECIMAL - 1.9998 :: DECIMAL);
+
+--> 05: Character Type : https://github.com/anand-kumar96/sqlandPostgreSqlNotes/assets/106487247/858d13fb-d132-4827-bbe2-988a93e6eae3
+        SELECT ('CHCHSCHShh':: CHAR(3)) --> CHC
+        SELECT ('C':: CHAR(3)) -->C  --> two spaces automatically added right of C
+    --> CHAR(N)=> n length of character
+    --> VARCHAR => Store any length of string
+    --> VARCHAR(50) => Store a string upto 50 characters automatically remove extra characters
+    --> TEXT => Store any length of String
+
+--> 06: Boolean Data Types : https://github.com/anand-kumar96/sqlandPostgreSqlNotes/assets/106487247/9fccd886-74a5-428d-890f-d2f58d5e352a
+    --> true,yes,on,1,t,y ---> TRUE, false,no,off,0,f,n --> FALSE, null --> NULL
+        SELECT('yes':: Boolean), SELECT('y':: Boolean), SELECT('1':: Boolean), SELECT('t':: Boolean), SELECT('on':: Boolean) -->TRUE
+        SELECT('no':: Boolean), SELECT('n':: Boolean), SELECT('0':: Boolean), SELECT('f':: Boolean), SELECT('off':: Boolean) -->FALSE
+        SELECT(null:: Boolean) --> NULL
+
+--> 07: Times, Date and TimesStamp : https://github.com/anand-kumar96/sqlandPostgreSqlNotes/assets/106487247/ee36e3c8-2c76-49a7-ac46-8933f208358c
+        
