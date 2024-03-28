b0017cab2f2afe37805877167d14e3f988f684a1
Anand Kumar <hyeiamaka06@gmail.com>
2024-03-28 18:09:23 +0530

    updated

--- 27Recursive Common Table Expressions.sql
+++ 27Recursive Common Table Expressions.sql
@@ -34,5 +34,35 @@
         FROM countdown
         WHERE val > 1
         )
+ 
+--> 02: Step by Step Explanation
+        WITH RECURSIVE countdown(val) AS (
+        SELECT 40 AS val --> Initialor Non Recursive Query
+        UNION ALL
+        SELECT val - 1 FROM countdown WHERE val > 1 --> Recursive Query
+        )
+    --> https://github.com/anand-kumar96/sqlandPostgreSqlNotes/assets/106487247/83d932f8-471a-49f2-a251-2f32c214eeac
+    --> Define the results table and working table seperately with the name mention i.e. val
+    --> then run the non-recursive statement : 
+        SELECT 3 AS val
+    --> put the results of it in result and working table i.e val as 3
+    --> now run recursive statement 
+        SELECT val - 1 FROM countdown WHERE val > 1
+    --> replace the countdown  table referencing to working table
+    --> now execute it
+    --> https://github.com/anand-kumar96/sqlandPostgreSqlNotes/assets/106487247/5c7e8c4e-18ca-4bc0-b7d7-de4a27f446c7
 
---> 02: 
+--> 03: Why We use Recursive CTE ?
+    --> How Suggestion work in instagram and facebook : Suppose I am following The Rock and Kevin Hart , 
+    --> while The Rock following justinbieber and Kevin Hart following jennifer lopez and Snoop Dogg.
+    --> then instagram think or make assumption i am interested in Justin Beiber a, Jennifer lopez and Snoop Dogg.
+    --> and continue one level deeper etc
+    --> https://github.com/anand-kumar96/sqlandPostgreSqlNotes/assets/106487247/8c670625-6aab-4d65-80cc-5214c3301224
+    --> how can we do that ? : We can do using join statement but there is issue we can not go deep and deep means 
+    --> x--follow--> y --follow--> z--follow--> a
+    --> means we can not get all the following followers so we will use Recursive CTC.
+    --> https://github.com/anand-kumar96/sqlandPostgreSqlNotes/assets/106487247/e5d425a1-c162-4f23-ad57-ab8ad48c67a3
+    --> Since above image also showing tree type structure so use Recursive CTE.
+
+--> 04: 
+    
