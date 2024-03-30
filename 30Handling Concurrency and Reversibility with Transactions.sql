694c38aab365aef3d5053a73929a961c2710c165
Anand Kumar <hyeiamaka06@gmail.com>
2024-03-30 11:38:03 +0530

    updated

--- 30Handling Concurrency and Reversibility with Transactions.sql
+++ 30Handling Concurrency and Reversibility with Transactions.sql
@@ -40,13 +40,41 @@
     --> in this connection we get
       /* id    name   balance   
           1    Gia       100
-          2    Alyson     50
+          2    Alyson    50
       */
 
     --> in 2nd query tool execute 
         SELECT * FROM accounts;
       /* id    name   balance   
           1    Gia       100
-          2    Alyson     100
+          2    Alyson    100
       */
     --> Because both query are executing in different enviroment.
+    /*
+        Transactional Control Commands:
+        Transactional control commands are only used with the DML Commands such as - INSERT, UPDATE and DELETE. 
+        They cannot be used while creating tables or dropping them because these operations are automatically 
+        committed in the database. Following commands are used to control transactions.
+
+        COMMIT − to save the changes.
+        ROLLBACK − to roll back the changes.
+        SAVEPOINT − creates points within the groups of transactions in which to ROLLBACK.
+        SET TRANSACTION − Places a name on a transaction.
+    */
+    --> https://github.com/anand-kumar96/sqlandPostgreSqlNotes/assets/106487247/c529a5aa-0310-4f7a-ba40-35e8a49bd12b
+    --> 1 COMMIT : to merge changes back into main pool.
+        /*
+        The COMMIT command is the transactional command used to save changes invoked by a transaction. 
+        It saves all the transactions occurred on the database since the last COMMIT or ROLLBACK.
+        */
+    --> 2 ROLLBACK: used to dump all pending changes and delete the seperate workspace.
+    --> https://github.com/anand-kumar96/sqlandPostgreSqlNotes/assets/106487247/bcacfb90-872b-4cba-8aee-f7afef3d06aa
+        /*
+        The ROLLBACK command is the transactional command used to undo transactions that have not already been saved to the database. 
+        This command can only undo transactions since the last COMMIT or ROLLBACK.
+        */
+    --> 3 Running a bad command will put the transaction in an 'aborted' state - you must rollback. in such situation
+    --> https://github.com/anand-kumar96/sqlandPostgreSqlNotes/assets/106487247/2f2f3fd8-f802-4d27-a90b-21272ffae2e6
+
+    --> Losing the connection (crashing) willautomatically rollback the transaction
+    --> https://github.com/anand-kumar96/sqlandPostgreSqlNotes/assets/106487247/7f1688f6-ee14-4e70-bd0a-203cc493325a
