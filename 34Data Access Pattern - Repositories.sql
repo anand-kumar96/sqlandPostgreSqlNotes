6b0a9de471e905049792d4fb8d12450d6ec5136a
Anand Kumar <hyeiamaka06@gmail.com>
2024-04-02 18:21:41 +0530

    updated

---
+++ 34Data Access Pattern - Repositories.sql
@@ -0,0 +1,18 @@
+--> 01: The Repository Pattern
+    --> https://github.com/anand-kumar96/sqlandPostgreSqlNotes/assets/106487247/b4f26c74-385a-4bc3-bd24-5b1849043548
+    --> https://github.com/anand-kumar96/sqlandPostgreSqlNotes/assets/106487247/bf6f092d-7eeb-4b49-88bc-2c3fddc5026e
+    --> there are various way to implement it, as object with plain function, as an instance of a class, as a class with static methods anything.
+
+--> 02: Creating a repository
+    --> various way to create 
+
+--> 03: Accessing the API (Postman, Thunder client, Rest client)
+    --> 1 using postman : i already know
+    --> 2 using thunder client : Already Know
+
+    --> 3 using Rest client : add restclient extension in vs code and create a file request.hhtp
+    --> now paste url and right click and just send request to send another write ### then another url
+        http://localhost:3005/users
+
+        ###
+        http://localhost:3005/users
